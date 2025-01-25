448ec1604bcde779308ef895583b657890600e50
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-01-25 14:56:21 +0600

    Welcome to Nux E-Com

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-gray-700 text-4xl">Welcome to <span class="text-blue-900 text-6xl">Nux E-Com</span></h1>
   </div>
 </template>
