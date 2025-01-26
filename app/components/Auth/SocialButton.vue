51e7175ae270c8dec883169831b8c5cdf136e527
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-01-27 00:37:38 +0600

    Designed Login and Register Page

---
+++ app/components/Auth/SocialButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <Button type="button" variant="outline">
+            <div class="flex items-center">
+                <Icon :icon="icon" class="mr-1 h-4 w-4"></Icon> {{ label }}
+            </div>
+        </Button>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+defineProps<{
+    label: string,
+    icon: string
+}>()
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+<template>
+        <div class="flex justify-center items-center h-screen">
+            <h1 class="text-gray-700 text-4xl">Welcome to <span class="text-blue-900 text-6xl">Nux E-Com</span></h1>
+        </div>
+        <NuxtLink to="/auth/login" class="text-blue-900">Login</NuxtLink>
+        <NuxtLink to="/auth/register" class="text-blue-900">Register</NuxtLink>
+</template>
